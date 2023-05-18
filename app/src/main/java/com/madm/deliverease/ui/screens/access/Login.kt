11f684b82bfae6688408912298abc4b69195b833
Damiano <damiano.imola@outlook.it>
2023-05-18 18:11:28 +0200

    - calendar done

--- app/src/main/java/com/madm/deliverease/ui/screens/access/Login.kt
+++ app/src/main/java/com/madm/deliverease/ui/screens/access/Login.kt
@@ -1,35 +1,24 @@
 package com.madm.deliverease.ui.screens.access
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.runtime.*
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.madm.deliverease.MainActivity
 import com.madm.deliverease.R
-import com.madm.deliverease.ui.theme.*
+import com.madm.deliverease.ui.theme.largePadding
+import com.madm.deliverease.ui.theme.mediumPadding
 import com.madm.deliverease.ui.widgets.LoginButton
 import com.madm.deliverease.ui.widgets.MyButton
 import com.madm.deliverease.ui.widgets.MyOutlinedTextField
@@ -70,9 +59,9 @@ fun LoginScreen(
 
 @Composable
 fun ClassicLogin(goToRiderHome: () -> Unit, goToAdminHome: () -> Unit) {
-    var username = remember { mutableStateOf("") }
-    var password = remember { mutableStateOf("") }
-    var isError = remember { mutableStateOf(false) }
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val isError = remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
 
     Column {
--- app/src/main/java/com/madm/deliverease/ui/screens/riders/CalendarScreen.kt
+++ app/src/main/java/com/madm/deliverease/ui/screens/riders/CalendarScreen.kt
@@ -1,9 +1,301 @@
 package com.madm.deliverease.ui.screens.riders
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.madm.deliverease.ui.theme.mediumPadding
+import com.madm.deliverease.ui.theme.nonePadding
+import com.madm.deliverease.ui.theme.smallPadding
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
+import java.util.*
+
+
+val MonthMap = mapOf(
+    0 to "January",
+    1 to "February",
+    2 to "March",
+    3 to "April",
+    4 to "May",
+    5 to "June",
+    6 to "July",
+    7 to "August",
+    8 to "September",
+    9 to "October",
+    10 to "November",
+    11 to "December"
+)
+
+
+data class Day(val number: Int, val name: String)
+
+fun getWeekDays(year: Int, month: Int, week: Int): List<Day> {
+    val firstDayOfMonth = LocalDate.of(year, month, 1)
+    val firstDayOfWeek = firstDayOfMonth.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY))
+        .plusWeeks(week.toLong() - 1)
+
+    val days = mutableListOf<Day>()
+    var currentDate = firstDayOfWeek
+
+    for (i in 0 until 7) {
+        val dayNumber = currentDate.dayOfMonth
+        val dayName = currentDate.dayOfWeek.getDisplayName(java.time.format.TextStyle.FULL, Locale.getDefault())
+        days.add(Day(dayNumber, dayName))
+        currentDate = currentDate.plusDays(1)
+    }
+
+    return days
+}
+
+fun getMondays(year: Int, month: Int): List<Int> {
+    val firstOfMonth = LocalDate.of(year, month, 1)
+    val lastOfMonth = LocalDate.of(year, month, 1).with(TemporalAdjusters.lastDayOfMonth())
+
+    val mondays = mutableListOf<Int>()
+    var currentDate = firstOfMonth.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY))
+
+    while (currentDate.isBefore(lastOfMonth) || currentDate.isEqual(lastOfMonth)) {
+        mondays.add(currentDate.dayOfMonth)
+        currentDate = currentDate.with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+    }
+
+    return mondays
+}
+
+fun Int.integerToTwoDigit() : String {
+    return if(this < 10)
+        "0$this"
+    else "$this"
+}
+
+
+
+
+
+
+
 
 @Composable
 fun CalendarScreen(){
-    Text(text = "RIDERS Calendar section")
+    var indexOfSelectedWeek : Int by remember { mutableStateOf(1) }
+    val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+    val months = (currentMonth..currentMonth + 12)
+        .toList()
+        .toIntArray()
+        .map { i -> MonthMap[i%12]!! }
+    var selectedMonth by remember { mutableStateOf(months[0]) }
+
+
+    Column(){
+        MonthSelector(months, selectedMonth) { item: String -> selectedMonth = item }
+        WeeksList(currentMonth, currentYear) { weekNumber: Int -> indexOfSelectedWeek = weekNumber }
+        WeekShifts(indexOfSelectedWeek, currentMonth, currentYear)
+    }
 }
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MonthSelector(months: List<String>, selectedMonth: String, function: (String) -> Unit){
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded}
+    ) {
+        TextField(
+            value = "$selectedMonth",
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier.width(IntrinsicSize.Min)
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            months.forEach { selectedOption ->
+                // menu item
+                DropdownMenuItem(
+                    onClick = {
+                        function(selectedOption)
+                        expanded = false
+                    }
+                ) {
+                    Text(text = selectedOption)
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun WeeksList(selectedMonth: Int, selectedYear: Int, function: (Int) -> Unit) {
+    var weekNumber = 1
+    val mondayList = getMondays(selectedYear, selectedMonth+1)
+
+    val weeks = mondayList
+        .toList()
+        .toIntArray()
+        .map {
+            i -> "${i.integerToTwoDigit()} ${MonthMap[selectedMonth]!!}"
+        }
+
+    var selectedWeek by remember { mutableStateOf(weeks[0]) }
+    var lastDayOfWeek by remember { mutableStateOf(
+            (Integer.parseInt(selectedWeek.subSequence(0..1) as String) + 6).toString()
+        )
+    }
+    val remainderString by remember {
+        mutableStateOf(selectedWeek.subSequence(2 until selectedWeek.length))
+    }
+
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .horizontalScroll(rememberScrollState())
+    ){
+        weeks.forEach {
+            Button(
+                onClick = {
+                    function(weekNumber)
+                    selectedWeek = it
+                    lastDayOfWeek = (Integer.parseInt(it.subSequence(0..1) as String) + 6).toString()
+                    weekNumber += 1
+                },
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 6.dp,
+                    pressedElevation = 8.dp,
+                    disabledElevation = 2.dp
+                ),
+                modifier = Modifier
+                    .padding(smallPadding, smallPadding)
+                    .clip(shape = RoundedCornerShape(20)),
+                colors = ButtonDefaults.buttonColors(
+                    if (selectedWeek == it) Color(0xFFFF9800)
+                    else Color(0xFFFF5722)
+                ),
+                border = BorderStroke(width = 1.dp, color = Color.Red),
+                shape = RoundedCornerShape(20)
+            ) {
+                Text(
+                    text = it,
+                    color = Color.White
+                )
+            }
+        }
+    }
+
+    Row (
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Start
+    ){
+        Text(
+            text = "Week: ",
+            style = TextStyle(
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Normal
+            )
+        )
+        Text(
+            text = "$selectedWeek - $lastDayOfWeek $remainderString",
+            style = TextStyle(
+                fontSize = 25.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(0xFFFF9800)
+            )
+        )
+    }
+
+    Row {
+        Text(text = "You have")
+        Text(
+            text = " 3 ",
+            style = TextStyle(
+                color = Color(0xFFFF9800),
+                fontWeight = FontWeight.Bold
+            ),
+        )
+        Text(text = " shifts assigned")
+    }
+}
+
+
+@Composable
+fun WeekShifts(weekNumber: Int, selectedMonth: Int, selectedYear: Int){
+    println("####### $selectedYear $selectedMonth")
+    val days = getWeekDays(selectedYear, selectedMonth+1, weekNumber)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .padding(top = mediumPadding)
+            .fillMaxHeight()
+    ) {
+        days.forEach { 
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(nonePadding, smallPadding)
+            ) {
+                Text(text = "${it.number} ${it.name}")
+                Divider(modifier = Modifier
+                    .fillMaxWidth()
+                    .width(2.dp)
+                    .padding(start = smallPadding))
+            }
+
+            val currentRandomVal = (0..1).random()
+            Row(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(20))
+                    .background(
+                        color = if (currentRandomVal == 0) Color.LightGray else Color(
+                            0xFFFF9800
+                        )
+                    )
+                    .fillMaxWidth()
+                    .padding(smallPadding)
+            ){
+                Text(
+                    text = if (currentRandomVal == 0) "No shift" else "You have a shift!",
+                    style = TextStyle(color = Color.White)
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
