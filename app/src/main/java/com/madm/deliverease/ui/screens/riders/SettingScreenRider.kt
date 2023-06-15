1530d915168c69a82fba015d7b67d20e9daa9e52
Alessandro <alessandro.finocchi@students.uniroma2.eu>
2023-06-15 09:58:11 +0200

    fix issue #13 (still to test when server is off= fix issue #30

--- app/src/main/java/com/madm/deliverease/ui/screens/riders/SettingScreenRider.kt
+++ app/src/main/java/com/madm/deliverease/ui/screens/riders/SettingScreenRider.kt
@@ -1,12 +1,10 @@
 package com.madm.deliverease.ui.screens.riders
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.madm.deliverease.ui.widgets.PreferencesSetting
@@ -14,10 +12,12 @@ import com.madm.deliverease.ui.widgets.PreferencesSetting
 @Composable
 fun SettingScreenRider(logoutCallback: () -> Unit){
     Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
-            .verticalScroll(rememberScrollState()).padding(vertical = 8.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState()),
     ) {
         PreferencesSetting(logoutCallback)
 
